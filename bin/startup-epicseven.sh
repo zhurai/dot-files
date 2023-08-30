b0520b1ef27eda1fab75bcd6a5169fc42a5701c9
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-08-29 22:23:42 -0700

    update waydroid/e7 startup script to move the weston window to 0,0

--- bin/startup-epicseven.sh
+++ bin/startup-epicseven.sh
@@ -4,6 +4,7 @@ waydroid session stop
 sleep 3 
 weston --width=1280 --height=720 &
 sleep 2
+xdotool search --class "weston" windowmove -- 0 0 &
 WAYLAND_DISPLAY=wayland-1 waydroid session start &
 sleep 2
 WAYLAND_DISPLAY=wayland-1 waydroid show-full-ui &
