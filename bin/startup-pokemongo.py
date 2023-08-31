0492502ed16252790b32e0eb5c97d0c7f4aa1fff
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-08-31 14:58:24 -0700

    add handling for setting the window in pogo script

--- bin/startup-pokemongo.py
+++ bin/startup-pokemongo.py
@@ -8,6 +8,8 @@ game="Pokemon Go"
 gamepkg0="com.cjin.pokegenie.standard"
 gamepkg1="com.nianticlabs.pokemongo"
 search="OnePlus9" # this would have to change if it is a different device/model for others!
+titlefilter="^zhurai-phone$" # others may want to change this
+starttitle="zhurai-phone - "+game # others may want to change this
 
 # make sure the device is connected to adb and everything is enabled first
 # also make sure you "Connect Network or Shared Device" to the device on Vysor first
@@ -21,8 +23,10 @@ device=deviceawk.stdout.decode('utf-8').strip()
 subprocess.Popen(["vysorapp","-s",device],close_fds=True)
 time.sleep(10)
 
-# open the game
+# open pokegenie
 subprocess.Popen(["adb","-s",device,"shell","monkey","-p",gamepkg0,"1"])
+subprocess.Popen(["xdotool","search","-name", titlefilter,"set_window","--name",starttitle]) 
 time.sleep(10)
 
-#subprocess.Popen(["adb","-s",device,"shell","monkey","-p",gamepkg1,"1"])
+# open game directly
+subprocess.Popen(["adb","-s",device,"shell","monkey","-p",gamepkg1,"1"])
