1e4fc766bf7cd3c10389b2680a654e1bff01595f
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-09-28 17:36:46 -0700

    not using waydroid for arknights

--- bin/inactive/startup-arknights.sh
+++ bin/inactive/startup-arknights.sh
@@ -3,7 +3,6 @@
 GAME="Arknights"
 WIDTH=1920
 HEIGHT=1011
-TEMPFILE=/tmp/z.waydroid.${GAME,,}
 APKPKG="com.YoStarEN.Arknights"
 
 # make sure any previous waydroid sessions are stopped
@@ -16,24 +15,28 @@ weston --width=${WIDTH} --height=${HEIGHT} &
 sleep 2
 
 # once the weston window in X11 has appeared, move weston to 0,0, and start a new waydroid session
+WAYLAND_DISPLAY=wayland-1 waydroid session start &
 xdotool search --class "weston" windowmove -- 0 0 &
 xdotool search --class "weston" set_window --name "Waydroid - ${GAME}" &
-WAYLAND_DISPLAY=wayland-1 waydroid session start &
-sleep 2
+sleep 10 # handle race condition
 
 # once the session should have been started, show the ui
 WAYLAND_DISPLAY=wayland-1 waydroid show-full-ui &
-sleep 2
+sleep 5
 
 # Start custom service to turn off/restart waydroid
 systemctl --user daemon-reload
 systemctl --user start waydroid-check.service
 
 # System seems to have started fully by now
-sleep 10 # if you don't have a delay there would be a race condition where it would try to launch the app while the OS is booting, which would just cause a crash
-timeout 1 waydroid log > ${TEMPFILE}
-DEVICE=$(cat ${TEMPFILE} | grep ADB | tail -n 1 | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | sed 's/$/:5555/g')
+sleep 10 # handle race condition
+
+# Check ADB information, set title
+DEVICE=$(waydroid status | grep IP | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | sed 's/$/:5555/g')
+WINDOWNAME=$(xdotool search --class "weston" getwindowname)
+xdotool search --class "weston" set_window --name "${WINDOWNAME} - ${DEVICE} - Initial" &
+
+# Launch/Connect
 adb connect ${DEVICE} 
-WINDOW=$(xdotool search --class "weston" getwindowname)
-xdotool search --class "weston" set_window --name "${WINDOW} - ${DEVICE} - Initial" &
-waydroid app launch ${APKPKG}
+adb -s ${DEVICE} shell settings put global http_proxy :0 
+#waydroid app launch ${APKPKG} &
