01fba80ed4ac28335a06e1eb9816b010b6574b60
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-09-30 03:13:58 -0700

    awesome.autostart: add background apps

---
+++ awesome/autostart/background.lua
@@ -0,0 +1,8 @@
+local awful = require("awful")
+
+awful.util.spawn("/usr/bin/slack")
+awful.util.spawn("/usr/bin/discord")
+awful.util.spawn("/usr/bin/remmina")
+awful.util.spawn("/usr/bin/autokey-gtk")
+awful.util.spawn("/usr/bin/steam")
+awful.util.spawn("/usr/bin/bitwarden-desktop")
