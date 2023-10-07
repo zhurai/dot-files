9ad145721593b332e7b971eed9ae2014e15dd62b
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 06:02:34 -0700

    sway: add the other keybinds for arknights

---
+++ bin/arknights/arknights.raiding.pause2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+curr_window=$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select (.pid and .visible) | {name}' | jq '.[]' | sed 's/[",]//g')
+title=$(swaymsg -t get_tree | grep Genymotion | grep Arknights | grep title | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
+adb_device=$(echo $title | cut -d "-" -f 2 | sed 's/^[[:space:]]*//g')
+
+if [[ "$curr_window" == "$title" ]]; then
+  if [[ -f "/tmp/zhurai-arknights.tmp" ]]; then
+    output=$(cat /tmp/zhurai-arknights.tmp | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
+    if [[ "$output" == *"Raiding-Standard"* ]]; then
+      if [ -n "$adb_device" ]; then
+        # range 1454,34 -> 1541,99
+        x=$(shuf -i 1454-1541 -n 1)
+        y=$(shuf -i 34-99 -n 1)
+        adb -s $adb_device shell input tap $x $y
+        x=$(shuf -i 1454-1541 -n 1)
+        y=$(shuf -i 34-99 -n 1)
+        adb -s $adb_device shell input tap $x $y
+      fi
+    elif [[ "$output" == *"Raiding-Alternate"* ]]; then
+      if [ -n "$adb_device" ]; then
+        # range 1454,34 -> 1541,99
+        x=$(shuf -i 1454-1541 -n 1)
+        y=$(shuf -i 34-99 -n 1)
+        adb -s $adb_device shell input tap $x $y
+        x=$(shuf -i 1454-1541 -n 1)
+        y=$(shuf -i 34-99 -n 1)
+        adb -s $adb_device shell input tap $x $y
+      fi
+    fi
+  fi
+fi
