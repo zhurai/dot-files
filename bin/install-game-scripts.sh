7084e0590820b3f96e8e6f760c54d5d98586e7ed
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 07:28:15 -0700

    update game install script for gfl

--- bin/install-game-scripts.sh
+++ bin/install-game-scripts.sh
@@ -2,9 +2,10 @@
 
 mkdir ~/.bin/arknights/
 mkdir ~/.bin/games/
+mkdir ~/.bin/girlsfrontline/
 rm -rv ~/.bin/arknights/*
 rm -rv ~/.bin/games/*
-#rm -rv ~/.bin/girlsfrontline/*
+rm -rv ~/.bin/girlsfrontline/*
 
 workingdir=$(dirname $(realpath "$0"))/arknights
 find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/arknights \; -exec echo {} \;
@@ -12,5 +13,5 @@ find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec l
 workingdir=$(dirname $(realpath "$0"))/games
 find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/games \; -exec echo {} \;
 
-#workingdir=$(dirname $(realpath "$0"))/girlsfrontline
-#find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/girlsfrontline \; -exec echo {} \;
+workingdir=$(dirname $(realpath "$0"))/girlsfrontline
+find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/girlsfrontline \; -exec echo {} \;
