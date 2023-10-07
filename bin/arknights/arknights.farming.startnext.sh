2c2665998cf21cae337b0e55ec0ba01c824ece38
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 03:14:39 -0700

    bin: add arknights farming keybind script

---
+++ bin/arknights/arknights.farming.startnext.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curr_window=$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select (.pid and .visible) | {name}' | jq '.[]' | sed 's/[",]//g')
+title=$(swaymsg -t get_tree | grep Arknights | grep title | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
+adb_device=$(echo $title | cut -d "-" -f 2 | sed 's/^[[:space:]]*//g')
+
+if [[ "$curr_window" == "$title" ]]; then
+  if [ -n "$adb_device" ]; then
+    adb -s $adb_device shell input tap 1454 785 
+  fi
+fi
