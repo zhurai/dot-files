b577f6603a33e3cb0bd086e4aed9ba793a8d5ba5
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 07:31:38 -0700

    systemd: add user argument

--- bin/install-systemd-user.sh
+++ bin/install-systemd-user.sh
@@ -16,8 +16,8 @@ echo $workingdir2
 find $workingdir2 -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.config/systemd/user \; -exec echo {} \;
 
 # reload daemon
-systemctl daemon-reload
+systemctl --user daemon-reload 
 
 # enable timers needed
-systemctl enable --now end-blue-light-filter.timer
-systemctl enable --now start-blue-light-filter.timer
+systemctl --user enable --now end-blue-light-filter.timer
+systemctl --user enable --now start-blue-light-filter.timer
