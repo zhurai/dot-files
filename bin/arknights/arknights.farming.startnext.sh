9a54b5b35b63b08d6edc6d3ae50170fa942ab35b
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 04:57:11 -0700

    sway.keybind: update arknights farming script to detect mode

--- bin/arknights/arknights.farming.startnext.sh
+++ bin/arknights/arknights.farming.startnext.sh
@@ -4,7 +4,12 @@ title=$(swaymsg -t get_tree | grep Arknights | grep title | awk '{$1=""}1' | sed
 adb_device=$(echo $title | cut -d "-" -f 2 | sed 's/^[[:space:]]*//g')
 
 if [[ "$curr_window" == "$title" ]]; then
-  if [ -n "$adb_device" ]; then
-    adb -s $adb_device shell input tap 1454 785 
+  if [[ -f "/tmp/zhurai-arknights.tmp" ]]; then
+    output=$(cat /tmp/zhurai-arknights.tmp | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
+    if [[ "$output" == *"Farming"* ]]; then
+      if [ -n "$adb_device" ]; then
+        adb -s $adb_device shell input tap 1454 785 
+      fi
+    fi
   fi
 fi
