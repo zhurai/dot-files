3a6bd6c44ca77e3d68740bd36ae6780d14826e3b
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 03:14:57 -0700

    bin: add install-game-scripts

---
+++ bin/install-game-scripts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir ~/.bin/arknights/
+rm -rv ~/.bin/arknights/*
+#rm -rv ~/.bin/girlsfrontline/*
+
+workingdir=$(dirname $(realpath "$0"))/arknights
+find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/arknights \; -exec echo {} \;
+
+#workingdir=$(dirname $(realpath "$0"))/girlsfrontline
+#find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/girlsfrontline \; -exec echo {} \;
