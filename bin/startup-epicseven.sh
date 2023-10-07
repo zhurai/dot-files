50f8718470b5ec5a5ebddf036e37f2b5f2385b67
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-07 04:56:47 -0700

    comment out x11 specific waydroid script stuff

--- bin/startup-epicseven.sh
+++ bin/startup-epicseven.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-GAME="EpicSeven"
-WIDTH=1920
-HEIGHT=1011
-TEMPFILE=/tmp/z.waydroid.${GAME,,}
-APKPKG=""
+#GAME="EpicSeven"
+#WIDTH=1920
+#HEIGHT=1011
+#TEMPFILE=/tmp/z.waydroid.${GAME,,}
+#APKPKG=""
 
 # make sure any previous waydroid sessions are stopped
-killall weston
+#killall weston
 waydroid session stop 
 sleep 3 
 
 # open weston wayland compositor
-weston --width=${WIDTH} --height=${HEIGHT} &
-sleep 2
+#weston --width=${WIDTH} --height=${HEIGHT} &
+#sleep 2
 
 # once the weston window in X11 has appeared, move weston to 0,0, and start a new waydroid session
-xdotool search --class "weston" windowmove -- 0 0 &
-xdotool search --class "weston" set_window --name "Waydroid - ${GAME}" &
+#xdotool search --class "weston" windowmove -- 0 0 &
+#xdotool search --class "weston" set_window --name "Waydroid - ${GAME}" &
 WAYLAND_DISPLAY=wayland-1 waydroid session start &
 sleep 2
 
@@ -27,13 +27,13 @@ sleep 2
 
 # Start custom service to turn off/restart waydroid
 systemctl --user daemon-reload
-systemctl --user start waydroid-check.service
+#systemctl --user start waydroid-check.service
 
 # System seems to have started fully by now
 sleep 10 # if you don't have a delay there would be a race condition where it would try to launch the app while the OS is booting, which would just cause a crash
 timeout 1 waydroid log > ${TEMPFILE}
 DEVICE=$(cat ${TEMPFILE} | grep ADB | tail -n 1 | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' | sed 's/$/:5555/g')
 adb connect ${DEVICE} 
-WINDOW=$(xdotool search --class "weston" getwindowname)
-xdotool search --class "weston" set_window --name "${WINDOW} - ${DEVICE} - Initial" &
+#WINDOW=$(xdotool search --class "weston" getwindowname)
+#xdotool search --class "weston" set_window --name "${WINDOW} - ${DEVICE} - Initial" &
 waydroid app launch ${APKPKG}
