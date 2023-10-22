c6a1a97d9ece4aa95b84c7671e81762993200eb7
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-22 07:32:21 -0700

    scripts: remove fribblese7optimizer script (could not find a stable way except running through terminal)

--- bin/startup-fribbles-e7-optimizer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /mnt/SSD1/Games/Lutris/fribbles-e7-optimizer
-cd "drive_c/Program Files/FribbelsE7Optimizer"
-WINEPREFIX=/mnt/SSD1/Games/Lutris/fribbles-e7-optimizer wine FribbelsE7Optimizer.exe
