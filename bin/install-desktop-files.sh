9cbaf1ce3513e73465173f86943ac74dd9f72a0d
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-10-27 22:50:50 -0700

    unify personal install/update script

--- bin/install-desktop-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this command not compatible with Epic Seven script $HOME even though it works otherwise in KDE
-#desktop-file-install --dir=$HOME/.local/share/applications `pwd`../application-files/*.desktop
-
-desktop="*.desktop"
-workingdir=$(dirname "$0")/../application-files
-find $workingdir/$desktop -exec chmod +x {} \; -exec cp {} ~/.local/share/applications/ \; -exec echo {} \;
-update-desktop-database ~/.local/share/applications
--- bin/install-personal-scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rv ~/.bin/*
-workingdir=$(dirname $(realpath "$0"))
-find $workingdir -maxdepth 1 -type f ! -name "*.md" -exec chmod +x {} \; -exec ln -s {} ~/.bin/ \; -exec echo {} \;
