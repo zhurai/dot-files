8def36ae9769fa34e656ef7aee9075f484e9c827
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-11-09 15:08:30 -0800

    scripts: update timer scripts to not use/check X11 status (bugging out with VNC updates

--- bin/timer/timer-start-blue-light-filter.sh
+++ bin/timer/timer-start-blue-light-filter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-session_type=$(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}')
-echo $session_type
+#session_type=$(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}')
+#echo $session_type
 
-if [[ "$session_type" == "wayland" ]]; then
-  killall -s 9 gammastep
-  WAYLAND_DISPLAY=wayland-1 gammastep -PO 1000 
-fi
+#if [[ "$session_type" == "wayland" ]]; then
+#  
+killall -s 9 gammastep
+#  WAYLAND_DISPLAY=wayland-1 gammastep -PO 1000 
+#fi
 
 # X11
 #Environment=DISPLAY=:0.0
