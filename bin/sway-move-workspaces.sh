b7bd5e1cd70afee8aec30192ffad9fafdd96c93a
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-11-09 17:07:50 -0800

    sway.kb: add related script

---
+++ bin/sway-move-workspaces.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curr=$(swaymsg -t get_outputs | jq '.[] | select(.focused==true) | .name' | sed 's|"||g')
+output0=$(swaymsg -t get_outputs --raw | jq '.[0].name' -r)
+output1=$(swaymsg -t get_outputs --raw | jq '.[1].name' -r)
+
+if [[ "$curr" == "$output0" ]]; then
+    swaymsg -rt get_workspaces | jq '.[]|.num' | xargs -I {} swaymsg workspace number {}, move workspace to output $output0
+elif [[ "$curr" == "$output1" ]]; then
+    swaymsg -rt get_workspaces | jq '.[]|.num' | xargs -I {} swaymsg workspace number {}, move workspace to output $output1
+fi
