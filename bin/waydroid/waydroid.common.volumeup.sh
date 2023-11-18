e1e7861dbb0a0595a471e2147d27b28c6d2ca477
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-11-17 23:05:37 -0800

    scripts: fix waydroid targetting

--- bin/waydroid/waydroid.common.volumeup.sh
+++ bin/waydroid/waydroid.common.volumeup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curr_window=$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select (.pid and .visible) | {app_id}' | jq '.[]' | sed 's/[",]//g')
-title=$(swaymsg -t get_tree | grep -i Waydroid | grep -vi Representation | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
+title=$(swaymsg -t get_tree | grep -i Waydroid | grep -vi Representation | grep -vi name | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
 adb_device=$(tail -n10 /tmp/z.waydroid.* | grep  "connected to"  | tail -n1 | sed 's/connected to //g' | sed 's/already //g')
 
 if [[ "$curr_window" == "$title" ]]; then
