56d5e9014fb38cda27af38b380e0b05125258f44
zhurai (Hana) <zhurai.tsuki@gmail.com>
2023-12-09 22:03:20 -0800

    scripts: update waydroid script to only save the ip, not the whole log

--- bin/waydroid/waydroid.common.volumedown.sh
+++ bin/waydroid/waydroid.common.volumedown.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 curr_window=$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select (.pid and .visible) | {app_id}' | jq '.[]' | sed 's/[",]//g')
 title=$(swaymsg -t get_tree | grep -i Waydroid | grep -vi Representation | grep -vi name | awk '{$1=""}1' | sed 's/^[[:space:]]*//g' | sed 's/[",]//g')
-adb_device=$(tail -n10 /tmp/z.waydroid.* | grep  "connected to"  | tail -n1 | sed 's/connected to //g' | sed 's/already //g')
+#adb_device=$(cat /tmp/z.waydroid.* | grep  "connected to"  | tail -n1 | sed 's/connected to //g' | sed 's/already //g')
+adb_device=$(cat /tmp/z.waydroid.*)
 
 if [[ "$curr_window" == "$title" ]]; then
   if [ -n "$adb_device" ]; then
