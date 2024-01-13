94d6fbd9f94fd8e5f7b3c5ebf89592b82fd0a026
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-01-13 05:46:55 -0800

    scripts: fix blue light filter timer scripts (ignoring X11 to clean up code)

--- bin/system.timer.bluelightfilter.end.sh
+++ bin/system.timer.bluelightfilter.end.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#session_type=$(loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}')
-#echo $session_type
-#
-#if [[ "$session_type" == "wayland" ]]; then
-#  
-killall -s 9 gammastep
-#  
-WAYLAND_DISPLAY=wayland-1 gammastep -PO 6500
-#fi
 
-# X11
-#Environment=DISPLAY=:0.0
-#ExecStart=redshift -PO 6500
+killall -s 9 gammastep
+sleep 1
+WAYLAND_DISPLAY=wayland-1 gammastep -PO 6500
