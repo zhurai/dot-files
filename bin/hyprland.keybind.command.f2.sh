c6fae7d093c565e0c35ed524f46a51dd548bd56f
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-02-04 00:24:48 -0800

    hyprland: add some other keybind hooks

---
+++ bin/hyprland.keybind.command.f2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This should set/toggle the current settings for the game
+
+# Confirm we are on Hyprland
+if [ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]; then 
+  exit
+fi
+
+# Confirm the window class
+window_title=$(hyprctl activewindow -j | jq '.title')
+window_class=$(hyprctl activewindow -j | jq '.class')
+
+# If Waydroid
+if [[ "$window_class" == "Waydroid" || "$window_class" == *"waydroid"* ]]; then
+  ~/.bin/hyprland.keybind.script.common.games.set_mode.sh
+
+# If Scrcpy
+elif [ "$window_class" == "scrcpy" ]; then
+  ~/.bin/hyprland.keybind.script.common.games.set_mode.sh
+
+# If Genymotion
+elif [ "$window_class" == "Genymotion Player" ]; then
+  ~/.bin/hyprland.keybind.script.common.games.set_mode.sh
+
+fi
