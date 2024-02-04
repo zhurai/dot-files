43b35e3586ea11476cfde942271779c79c712800
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-02-03 17:21:51 -0800

    scripts: hyprland: created volume control scripts for certain games

---
+++ bin/hyprland.keybind.command.page_up.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This should increase the volume for games, and ignore all others
+
+# Confirm we are on Hyprland
+if [ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]; then 
+  exit
+fi
+
+# Confirm the window class
+window_title=$(hyprctl activewindow -j | jq '.title')
+window_class=$(hyprctl activewindow -j | jq '.class')
+
+# If Waydroid
+if [[ "$window_class" == "Waydroid" || "$window_class" == *"waydroid"* ]]; then
+	~/.bin/hyprland.keybind.script.waydroid.common.volume_up.sh 
+fi
+
+# If Scrcpy
+if [ "$window_class" == "scrcpy" ]; then
+	~/.bin/hyprland.keybind.script.scrcpy.common.volume_up.sh
+fi
+
+# If Genymotion
+if [ "$window_class" == "Genymotion Player" ]; then
+	~/.bin/hyprland.keybind.script.genymotion.common.volume_up.sh
+fi
+
+
