69304172d53bb3c6ff0c1b63ecb909dc9286b7b4
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-02-03 21:56:25 -0800

    hyprland: kb: 2/w/s/x added for arknights (pause)

---
+++ bin/hyprland.keybind.command.s.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This should check the current settings for the game
+
+# Confirm we are on Hyprland
+if [ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]; then 
+  exit
+fi
+
+# Confirm the window class
+window_title=$(hyprctl activewindow -j | jq '.title')
+window_class=$(hyprctl activewindow -j | jq '.class')
+
+# If Waydroid
+if [[ "$window_class" == "Waydroid" || "$window_class" == *"waydroid"* ]]; then
+  waydroid_adb=$(cat /tmp/z.waydroid | head -n 1)
+  waydroid_game=$(cat /tmp/z.waydroid | tail -n 1)
+  if [[ "$waydroid_game" == "Arknights" ]]; then
+    ~/.bin/hyprland.keybind.script.waydroid.arknights.common.pause.sh
+  fi
+
+# If Scrcpy
+# elif [ "$window_class" == "scrcpy" ]; then
+
+# If Genymotion
+# elif [ "$window_class" == "Genymotion Player" ]; then
+
+fi
