77c77b42410ea9e5b712bf69e4fab6bea632bfdd
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-02-03 20:07:25 -0800

    script: add waydroid reset script

---
+++ bin/system.reset.waydroid.001.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Print the username: "
+read $the_user
+if [ "$EUID" -ne 0 ]
+  then echo "This script must be ran as root"
+  exit
+fi
+
+rm -rf /var/lib/waydroid /home/.waydroid
+rm -rf /home/${the_user}/waydroid /home/${the_user}/.share/waydroid /home/${the_user}/.local/share/applications/*aydroid* /home/${the_user}/.local/share/waydroid
+systemctl stop waydroid-container.service
+waydroid init -fs GAPPS 
+systemctl start waydroid-container.service
+cd /opt/waydroid-script/
+waydroid-extras -a 11 install libndk
