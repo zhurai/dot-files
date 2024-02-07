021cc3b471402d7497ce247776b8be1f350be5e8
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-02-07 00:54:17 -0800

    cleanup: remove stale scripts

--- bin/system.startup.chromium.chat.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-chromium --user-data-dir=$HOME/.config/chromium-chat --ozone-platform-hint=auto --enable-features=UseOzonePlatform --enable-gpu --ozone-platform=wayland --enable-webrtc-pipewire-capturer
