9691ce1a7783613d2fc3ac3a065b56db95596f19
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-02-07 07:11:20 -0800

    (missed a file)

---
+++ bin/system.startup.scrcpy-server001.epicseven.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+GAME="Epic Seven"
+TEMPFILE=/tmp/z.scrcpy-server001-b
+rm ${TEMPFILE}
+APKPKG="com.stove.epic7.google"
+TYPE="--tcpip"
+SERIAL="192.168.7.251:5555"
+TITLE="\"${GAME} - ${SERIAL}\""
+
+MODS="--shortcut-mod=lctrl+lalt"
+VIDEOCODEC="h264"
+#VIDEOENCODER="OMX.qcom.video.encoder.hevc"
+VIDEOBITRATE="25M"
+STAYAWAKE="--stay-awake"
+#SCREENOFF="--turn-screen-off"
+LEGACYPASTE="--legacy-paste"
+CLOSEPOWEROFF="--power-off-on-close"
+
+# make sure any previous sessions are stopped
+pkill -f -9 ${SERIAL}
+sleep 1
+
+# start a new session
+scrcpy --window-title="${TITLE}" ${TYPE}="${SERIAL}" --video-codec="${VIDEOCODEC}" --video-encoder="${VIDEOENCODER}" --video-bit-rate="${VIDEOBITRATE}" ${STAYAWAKE} ${MODS} ${SCREENOFF} ${LEGACYPASTE} ${CLOSEPOWEROFF} &
+sleep 1
+
+# System seems to have started fully by now
+echo "${SERIAL}" > ${TEMPFILE}
+echo "${GAME}" >> ${TEMPFILE}
+sleep 1
+adb -s ${SERIAL} shell monkey -p ${APKPKG} -c android.intent.category.LAUNCHER 1
