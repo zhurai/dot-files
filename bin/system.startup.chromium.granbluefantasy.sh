2b6aad528f34df3b3847e3301cd178a2c3968281
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-04-20 06:53:31 -0700

    scripts: startup gbf: vivaldi switch

--- bin/system.startup.chromium.granbluefantasy.sh
+++ bin/system.startup.chromium.granbluefantasy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-chromium --user-data-dir=$HOME/.config/chromium-gbf --enable-gpu --disable-backgrounding-occluded-windows --autoplay-policy=no-user-gesture-required --disable-background-timer-throttling --disable-renderer-backgrounding
-
-# --ozone-platform=wayland 
-# --ozone-platform-hint=auto 
-# --enable-features=UseOzonePlatform 
+vivaldi --user-data-dir=$HOME/.config/chromium-gbf --enable-gpu --disable-backgrounding-occluded-windows --autoplay-policy=no-user-gesture-required --disable-background-timer-throttling --disable-renderer-backgrounding --ozone-platform=wayland --ozone-platform-hint=auto --enable-features=UseOzonePlatform --hide-crash-restore-bubble
 
