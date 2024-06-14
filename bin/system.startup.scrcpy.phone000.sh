090012d8a54a80088082370b321a840c1bd26cad
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-14 06:29:58 -0700

    scripts.startup: scrcpy phone script modules add individual scrcpy arg variables

--- bin/system.startup.scrcpy.phone000.sh
+++ bin/system.startup.scrcpy.phone000.sh
@@ -2,7 +2,15 @@
 
 SERVER="Phone000"
 SERIAL="47f3c1bf"
+BITRATE="25M"
+_SCRCPYARG-MODKEY="--shortcut-mod=lctrl+lalt"
+_SCRCPYARG-BITRATE="--video-bit-rate=$BITRATE"
+_SCRCPYARG-AWAKE="--stay-awake"
+_SCRCPYARG-SCREENOFF="--turn-screen-off"
+_SCRCPYARG-LEGACYPASTE="--legacy-paste"
+_SCRCPYARG-POWEROFFONCLOSE="--power-off-on-close"
 
 . ~/.bin/system.startup.scrcpy.sh
 
 main
+
--- bin/system.startup.scrcpy.phone002.sh
+++ bin/system.startup.scrcpy.phone002.sh
@@ -2,6 +2,13 @@
 
 SERVER="Phone002"
 SERIAL="3446573236453498"
+BITRATE="25M"
+_SCRCPYARG-MODKEY="--shortcut-mod=lctrl+lalt"
+_SCRCPYARG-BITRATE="--video-bit-rate=$BITRATE"
+_SCRCPYARG-AWAKE="--stay-awake"
+_SCRCPYARG-SCREENOFF="--turn-screen-off"
+_SCRCPYARG-LEGACYPASTE="--legacy-paste"
+_SCRCPYARG-POWEROFFONCLOSE="--power-off-on-close"
 
 . ~/.bin/system.startup.scrcpy.sh
 
