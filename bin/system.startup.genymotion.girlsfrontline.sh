f35213e622edd1891811f76b52d8c82fbd0bfb6e
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-14 02:44:27 -0700

    scripts.startup: genymotion implement base script and for gfl

--- bin/system.startup.genymotion.girlsfrontline.sh
+++ bin/system.startup.genymotion.girlsfrontline.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 GAME="Girls Frontline"
-TEMPFILE=/tmp/z.genymotion.girlsfrontline
-rm ${TEMPFILE}
+GAMEFILE=$(echo ${GAME} | tr -d " \t\n\r" | tr '[:upper:]' '[:lower:]')
+GENYMOTIONNAME=$(echo ${GAME} | tr -d " \t\n\r")
 APKPKG="com.sunborn.girlsfrontline.en"
-UUID="84027d75-32d8-4315-b233-7a4b37b446be"
+UUID="f865c69e-cddc-48cb-aace-80d414060be1"
 
-# make sure any previous sessions are stopped
-pkill -f -9 ${UUID}
-sleep 5
+. ~/.bin/system.startup.genymotion.sh
 
-# start a new session
-gmtool admin start "${UUID}"
-sleep 30
-
-# System seems to have started fully by now
-TITLE=$(hyprctl clients -j | jq .[].title | grep "${GAME}")
-SERIAL=$(echo ${TITLE} | cut -f 2 -d '-' | awk '{$1=$1};1')
-echo "${SERIAL}" > ${TEMPFILE}
-echo "${GAME}" >> ${TEMPFILE}
-sleep 1
-adb -s ${SERIAL} shell monkey -p ${APKPKG} -c android.intent.category.LAUNCHER 1
-
-# todo: if error in starting up: kill the emulator and reboot
+main
---
+++ bin/system.startup.genymotion.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+TEMPFILE=/tmp/z.genymotion.${GAMEFILE}
+DE=${XDG_CURRENT_DESKTOP}
+
+# Stop previous sessions to reboot
+stop_sessions() {
+  
+  # remove temporary file
+  rm "${TEMPFILE}"
+
+  # stop sessions
+  pkill -f -9 ${UUID}
+  
+}
+
+# Start the new session
+start_sessions() {
+  
+  gmtool admin start "${UUID}"
+  
+}
+
+# Set up the temporary file
+populate_tempfile() {
+
+  if [[ "$DE" == "Hyprland" ]] then
+    TITLE=$(hyprctl clients -j | jq .[].title | grep "${GAME}")
+  elif [[ "$DE" == "sway" ]] then
+    TITLE=$(swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused==true).name')    
+  else
+    echo "Unknown Environment"
+    exit    
+  fi
+  
+  # From the window title, check the serial #
+  SERIAL=$(echo ${TITLE} | cut -f 2 -d '-' | awk '{$1=$1};1')
+  echo "${SERIAL}" > "${TEMPFILE}"
+  
+  # TODO: Potentially better to go with gmtool to prevent the need to check this step at least through the DE scripts?
+}
+
+# Start the game
+start_game() {
+  
+  # Send info to tempfile
+  echo "${GAME}" >> ${TEMPFILE}
+
+
+  # Launch APK if defined
+  if [[ -n "${APKPKG}" ]] then
+    adb -s ${SERIAL} shell monkey -p ${APKPKG} -c android.intent.category.LAUNCHER 1
+  fi
+
+}
+
+main() {
+
+  # Run coroutines
+  stop_sessions
+  sleep 5
+
+  start_sessions 
+  sleep 30
+
+  populate_tempfile
+  sleep 1
+  
+  start_game 
+}
