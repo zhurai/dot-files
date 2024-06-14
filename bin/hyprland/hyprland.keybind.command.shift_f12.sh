abadd1d3e77e32ac9689e08746893917db26f6ac
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-14 07:39:50 -0700

    rm temporary files

--- bin/hyprland/hyprland.keybind.command.shift_f12.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Confirm we are on Hyprland
-if [ "$XDG_CURRENT_DESKTOP" != "Hyprland" ]; then
-  exit
-fi
-
-AUDIODEVICENAME=$(pactl list | grep Source -A 10 | grep AT2020 | grep Name | awk '{print $2}')
-pactl set-source-mute $AUDIODEVICENAME toggle
--- bin/hyprland/hyprland.keybind.script.kill9-window.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curr_pid=$(hyprctl activewindow -j | jq ".pid")
-kill -9 ${curr_pid}
-
