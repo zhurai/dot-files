9b76cf1fb9d1108305f472060ddbc72a167a9506
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-14 06:54:41 -0700

    scripts.startup.chromium (fix): multiline typo

--- bin/system.startup.chromium.granbluefantasy.sh
+++ bin/system.startup.chromium.granbluefantasy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 vivaldi \
-  --user-data-dir=$HOME/.config/chromium-gbf
-  --window-name="Granblue Fantasy"
-  --enable-gpu
+  --user-data-dir=$HOME/.config/chromium-gbf \
+  --window-name="Granblue Fantasy" \
+  --enable-gpu \
 
-  --disable-backgrounding-occluded-windows
-  --autoplay-policy=no-user-gesture-required
-  --disable-background-timer-throttling
-  --disable-renderer-backgrounding
-  --hide-crash-restore-bubble
+  --disable-backgrounding-occluded-windows \
+  --autoplay-policy=no-user-gesture-required \
+  --disable-background-timer-throttling \
+  --disable-renderer-backgrounding \
+  --hide-crash-restore-bubble \
 
-  --ozone-platform=wayland
-  --ozone-platform-hint=auto
-  --enable-features=UseOzonePlatform
+  --ozone-platform=wayland \ 
+  --ozone-platform-hint=auto \ 
+  --enable-features=UseOzonePlatform \
+  " "
--- bin/system.startup.chromium.mahjongsoul.sh
+++ bin/system.startup.chromium.mahjongsoul.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 chromium \
-  --user-data-dir=$HOME/.config/chromium-majsoul 
-  --window-name="MahjongSoul - Chromium" 
-  --enable-gpu 
+  --user-data-dir=$HOME/.config/chromium-majsoul \
+  --window-name="MahjongSoul - Chromium" \
+  --enable-gpu \
 
-  --hide-crash-restore-bubble 
+  --hide-crash-restore-bubble \
 
-  --ozone-platform-hint=auto 
-  --enable-features=UseOzonePlatform 
-  --ozone-platform=wayland 
+  --ozone-platform-hint=auto \
+  --enable-features=UseOzonePlatform \
+  --ozone-platform=wayland \
 
   "https://mahjongsoul.game.yo-star.com/"
