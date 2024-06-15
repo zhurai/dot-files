a908114ae736f7fdff46291027983e4167554956
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-15 16:13:35 -0700

    script.startup: add Scrcpy Phone001

---
+++ bin/system.startup.scrcpy.phone001.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVER="Phone001"
+SERIAL="9889aa434350565943"
+BITRATE="25M"
+_SCRCPYARG_MODKEY="--shortcut-mod=lctrl+lalt"
+_SCRCPYARG_BITRATE="--video-bit-rate=$BITRATE"
+_SCRCPYARG_AWAKE="--stay-awake"
+_SCRCPYARG_SCREENOFF="--turn-screen-off"
+_SCRCPYARG_LEGACYPASTE="--legacy-paste"
+_SCRCPYARG_POWEROFFONCLOSE="--power-off-on-close"
+
+. ~/.bin/system.startup.scrcpy.sh
+
+main
