afc13ae21e9dff65d649b4cc1fd7b4a505a6d264
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-16 15:06:17 -0700

    scripts.keybind (chore) make the mode/profile check/adjustment more modular for minimal changes in the future if needed in both mode functions, but also to call and get the profile information in a single variable in other scripts

--- bin/system.keybind.common.games.mode.get.sh
+++ bin/system.keybind.common.games.mode.get.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-. ~/.bin/system.keybind.common.games.sh
-
-if [[ "$GAME" == *"Arknights"* ]] then
-  
-  PROFILE=$(sed -n '3p' "${CONFIG}")
-
-  if [[ -z "$PROFILE" ]] then
-    
-    echo "Initial" >> "${CONFIG}"
-    PROFILE=$(sed -n '3p' "${CONFIG}")
-
-  fi
-
-else
-  
-  echo "Unknown Player/Game Type"
-  exit
-
-fi
+. ~/.bin/system.keybind.common.games.mode.sh
 
 notify-send "$GAME" "$PROFILE"
---
+++ bin/system.keybind.common.games.mode.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+. ~/.bin/system.keybind.common.games.sh
+
+if [[ "$GAME" == *"Arknights"* ]] then
+  
+  PROFILE=$(sed -n '3p' "${CONFIG}")
+
+  if [[ -z "$PROFILE" ]] then
+    
+    echo "Initial" >> "${CONFIG}"
+    PROFILE=$(sed -n '3p' "${CONFIG}")
+    
+  else
+    
+    . ~/.bin/system.keybind.common.games.mode._arknights.sh
+
+  fi
+
+  OLDPROFILE="${PROFILE}"
+
+fi
+
+GAME_MODE_ADJUST() {
+  
+  for index in "${!PROFILE_LIST[@]}"; do
+  
+    if [[ "${PROFILE_LIST[$index]}" == "$PROFILE" ]] ; then
+      
+      if [[ "$index" == "$((${#PROFILE_LIST[@]}-1))" ]] ; then
+        
+        PROFILE="${PROFILE_LIST[0]}"
+
+      else
+      
+        PROFILE="${PROFILE_LIST[$(($index+1))]}"
+      
+      fi
+
+      sed -i "s|${OLDPROFILE}|${PROFILE}|" "${CONFIG}"
+      break
+
+    fi
+  
+  done
+
+}
+
