318bf98ad59efaa1af679cd7cbd0addbe9d93efc
zhurai (Hana) <zhurai.tsuki@gmail.com>
2024-06-26 16:57:15 -0700

    fix: scripts: very minor typo/formatting updates

--- bin/system.keybind.scripts.games.mode.sh
+++ bin/system.keybind.scripts.games.mode.sh
@@ -4,25 +4,22 @@
 
 if [[ "$GAME" == *"Arknights"* ]] then
   
+  . ~/.bin/system.keybind.common.arknights.sh
   PROFILE=$(sed -n '3p' "${CONFIG}")
-
+  
   if [[ -z "$PROFILE" ]] then
     
     echo "Initial" >> "${CONFIG}"
     PROFILE=$(sed -n '3p' "${CONFIG}")
-    
-  else
-    
-    . ~/.bin/system.keybind.common.arknights.sh
 
   fi
-
-  OLDPROFILE="${PROFILE}"
 
 fi
 
 GAME_MODE_ADJUST() {
   
+  OLDPROFILE="${PROFILE}"
+
   for index in "${!PROFILE_LIST[@]}"; do
   
     if [[ "${PROFILE_LIST[$index]}" == "$PROFILE" ]] then
