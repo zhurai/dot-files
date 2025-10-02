065dd8604cb07b52fdfbcf22063c95335f65390e
zhurai (Hana) <zhurai.tsuki@gmail.com>
2025-10-02 10:36:08 -0700

    hammerspoon: rework module names to be more consistent when scaling up

--- hammerspoon/dot-hammerspoon/init.lua
+++ hammerspoon/dot-hammerspoon/init.lua
@@ -2,8 +2,9 @@
 require("auto_reload")
 require("log_last_window")
 
-require("chrome_windows")
 require("chrome_forward")
 require("chrome_activate")
 
-require("vivaldi_gbf_windows")
+require("windowmgmt_vivaldi")
+require("windowmgmt_chrome")
+
