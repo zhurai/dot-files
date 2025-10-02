9ed5f33927b92e8b3f464386493ef27ca106a5e4
zhurai (Hana) <zhurai.tsuki@gmail.com>
2025-10-02 11:18:12 -0700

    hammerspoon.windowmgmt_other: update window positions

--- hammerspoon/dot-hammerspoon/windowmgmt_other.lua
+++ hammerspoon/dot-hammerspoon/windowmgmt_other.lua
@@ -1,7 +1,7 @@
 -- === SETTINGS ===
 local screenName = "ASUS"
-local terminalFrame = { x = 584, y = 0, w = 974, h = 639 }
-local feishinFrame = { x = 584, y = 0, w = 938, h = 639 }
+local terminalFrame = { x = 945, y = 0, w = 974, h = 639 }
+local feishinFrame = { x = 850, y = 0, w = 938, h = 639 }
 
 local windowFrames = {
 	["WezTerm"] = terminalFrame,
