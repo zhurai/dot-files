75260202f01baa7b6eedb267e88da23346389a5f
zhurai (Hana) <zhurai.tsuki@gmail.com>
2025-10-09 14:56:45 -0700

    hammerspoon: cleanup

--- hammerspoon/dot-hammerspoon/init.lua
+++ hammerspoon/dot-hammerspoon/init.lua
@@ -1,13 +1,10 @@
--- Require separate modules
 require("auto_reload")
 require("log_last_window")
 
 require("chrome_forward")
 
-require("activate_discord")
 require("activate_terminal")
 
---require("windowmgmt_vivaldi")
 require("windowmgmt_chrome")
 require("windowmgmt_firefox")
 require("windowmgmt_other")
