d02b8a08f0ff96063a411141d62379534e7f5ed7
zhurai (Hana) <zhurai.tsuki@gmail.com>
2025-11-03 23:26:04 -0800

    hammerspoon.singlescreen: adjustments made based on current plans

--- hammerspoon/dot-hammerspoon/singlescreen_handler.lua
+++ hammerspoon/dot-hammerspoon/singlescreen_handler.lua
@@ -3,19 +3,14 @@ layout_change = function()
         if #screens == 1 then
                 hs.application.get("Google Chrome"):kill()
                 hs.application.get("Discord"):kill()
-                hs.application.get("Slack"):kill()
                 hs.application.get("Feishin"):kill()
                 hs.application.get("Elements"):kill()
                 hs.application.get("Vivaldi"):kill()
         end        
-        hs.execute("open -na 'Google Chrome' --args --app='https://steamcommunity.com/chat/'")
         hs.execute("open -na 'Discord'")
-        hs.execute("open -na 'Slack'")
         hs.execute("open -na 'Feishin'")
         hs.execute("open -na 'Elements'")
         hs.execute("open -na 'Google Chrome' --args --app='https://www.messenger.com/'")
-        hs.execute("open -na 'Google Chrome' --args --app='https://www.twitch.tv/popout/zhurai/chat'")
-        hs.execute("open -na 'Vivaldi'")
 end
 
 local screenWatcher = hs.screen.watcher.new(layout_change)
